09ffc37cea6d34de5226f333f2e67ec24084230d
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-10 19:37:21 +0900

    baekjoon: 1550
    백준 1550: 16진수

---
+++ Bronze V/src/q1550/Q1550.java
@@ -0,0 +1,13 @@
+package q1550;
+
+import java.util.Scanner;
+
+public class Q1550 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String num = "";
+		num = sc.next();
+		sc.close();
+		System.out.println(Integer.parseInt(num, 16));
+	}
+}
