84aacc52705d390f9e6a48e990a649eb41ab208f
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-10 18:24:02 +0900

    baekjoon: 1001
    백준 1001: A-B

---
+++ Bronze V/src/q1001/Q1001.java
@@ -0,0 +1,14 @@
+package q1001;
+
+import java.util.*;
+
+public class Q1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 0, b = 0;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+		System.out.println(a - b);
+	}
+}
