a73cff737033faaba96864bf5ed89b4ad9d3cf7b
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-10 18:20:01 +0900

    backjoon: 1000
    백준 1000: A+B

---
+++ Bronze V/src/q1000/Q1000.java
@@ -0,0 +1,14 @@
+package q1000;
+
+import java.util.*;
+
+public class Q1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 0, b = 0;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+		System.out.println(a + b);
+	}
+}
