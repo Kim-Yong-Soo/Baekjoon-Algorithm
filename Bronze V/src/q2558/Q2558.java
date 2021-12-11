b63a4cc0358889d2525f7b5f3b3b735569b838a1
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-11 23:42:35 +0900

    baekjoon: 2558
    백준 2558: A+B - 2

---
+++ Bronze V/src/q2558/Q2558.java
@@ -0,0 +1,16 @@
+package q2558;
+
+import java.util.*;
+
+public class Q2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 0, b = 0;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+}
