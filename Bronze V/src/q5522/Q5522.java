f6bc02ce6fcf3a0560e104f2bdc3c598d0ca8f45
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-12 23:22:39 +0900

    baekjoon: 5522
    백준 5522: 카드 게임

---
+++ Bronze V/src/q5522/Q5522.java
@@ -0,0 +1,20 @@
+package q5522;
+
+import java.util.*;
+
+public class Q5522 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] score = new int[5];
+
+		for (int i = 0; i < score.length; i++) {
+			int input = sc.nextInt();
+			score[i] = (0 <= input || input <= 100) ? input : 0;
+		}
+
+		int sum = 0;
+		for (int sco : score)
+			sum += sco;
+		System.out.println(sum);
+	}
+}
