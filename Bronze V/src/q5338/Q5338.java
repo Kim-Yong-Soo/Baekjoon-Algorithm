a3264bc9bee4308ae7b60733e095a80fdee7142f
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-12 23:11:27 +0900

    baekjoon: 5338
    백준 5338: 마이크로소프트 로고

---
+++ Bronze V/src/q5338/Q5338.java
@@ -0,0 +1,7 @@
+package q5338;
+
+public class Q5338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+	}
+}
