e35850b9c29c28861416c0110eb87502fc240952
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-12 23:04:17 +0900

    baekjoon: 3046
    백준 3046: R2

---
+++ Bronze V/src/q3046/Q3046.java
@@ -0,0 +1,16 @@
+package q3046;
+
+import java.util.*;
+
+public class Q3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r1, s;
+
+		r1 = sc.nextInt();
+		s = sc.nextInt();
+		sc.close();
+
+		System.out.println(s * 2 - r1);
+	}
+}
