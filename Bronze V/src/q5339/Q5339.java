147355e34cdc35eacb8303934333c0cdb2ef9ae5
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-12 23:15:28 +0900

    baekjoon: 5339
    백준 5339: 콜센터

---
+++ Bronze V/src/q5339/Q5339.java
@@ -0,0 +1,7 @@
+package q5339;
+
+public class Q5339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+	}
+}
