bdaa7d2bda50c09c11cff27f0146af569eb85c42
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-14 22:34:34 +0900

    baekjoon: 10926
    백준 10926: ??!

---
+++ Bronze V/src/q10926/Q10926.java
@@ -0,0 +1,15 @@
+package q10926;
+
+import java.util.*;
+
+public class Q10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id;
+
+		id = sc.next();
+		sc.close();
+
+		System.out.println(id + "??!");
+	}
+}
