bfed44cd2cb1d702310ca611a65df5a1cb11091b
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-14 22:28:16 +0900

    baekjoon: 10757
    백준 10757: 큰 수 A+B

---
+++ Bronze V/src/q10757/Q10757.java
@@ -0,0 +1,17 @@
+package q10757;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Q10757 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a, b;
+
+		a = sc.nextBigInteger();
+		b = sc.nextBigInteger();
+		sc.close();
+
+		System.out.println(a.add(b));
+	}
+}
