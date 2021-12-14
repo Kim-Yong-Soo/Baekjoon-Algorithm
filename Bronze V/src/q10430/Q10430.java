066c6874b1d5d8be04454a797301c5f4d624c44b
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-14 22:12:39 +0900

    baekjoon: 10430
    백준 10430: 나머지

---
+++ Bronze V/src/q10430/Q10430.java
@@ -0,0 +1,20 @@
+package q10430;
+
+import java.util.*;
+
+public class Q10430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		sc.close();
+
+		System.out.println((a + b) % c);
+		System.out.println(((a % c) + (b % c)) % c);
+		System.out.println((a * b) % c);
+		System.out.println(((a % c) * (b % c)) % c);
+	}
+}
