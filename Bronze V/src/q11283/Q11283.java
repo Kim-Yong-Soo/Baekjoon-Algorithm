c9616e4bef032c0ef971aac0c141dc0cb2493ea4
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-14 22:52:25 +0900

    baekjoon: 11283
    백준 11283: 한글 2

---
+++ Bronze V/src/q11283/Q11283.java
@@ -0,0 +1,15 @@
+package q11283;
+
+import java.util.*;
+
+public class Q11283 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a;
+
+		a = sc.next().charAt(0);
+		sc.close();
+
+		System.out.println((int) a - 44031);
+	}
+}
