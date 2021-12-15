83e1fbe7b60a471c77783e49e0ade209ec2dfb46
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-15 23:21:13 +0900

    baekjoon: 11654
    백준 11654: 아스키 코드

---
+++ Bronze V/src/q11654/Q11654.java
@@ -0,0 +1,15 @@
+package q11654;
+
+import java.util.*;
+
+public class Q11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+
+		a = sc.next().charAt(0);
+		sc.close();
+
+		System.out.println(a);
+	}
+}
