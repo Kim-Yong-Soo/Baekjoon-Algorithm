3f9dd81553b3327a2292d727a5ba6d6cf665d938
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-15 23:33:43 +0900

    baekjoon: 10718
    UTF-8로 변경

--- Bronze V/src/q10718/Q10718.java
+++ Bronze V/src/q10718/Q10718.java
@@ -2,7 +2,7 @@ package q10718;
 
 public class Q10718 {
 	public static void main(String[] args) {
-		System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
-		System.out.println("°­ÇÑÄ£±¸ ´ëÇÑÀ°±º");
+		System.out.println("ê°•í•œì¹œêµ¬ ëŒ€í•œìœ¡êµ°");
+		System.out.println("ê°•í•œì¹œêµ¬ ëŒ€í•œìœ¡êµ°");
 	}
 }
