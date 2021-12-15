0aa4769d5f82bb4cac53ba916f4d116e24d097ab
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-15 23:58:57 +0900

    baekjoon: 13277
    백준 13277: 큰 수 곱셈

---
+++ Bronze V/src/q13277/Q13277.java
@@ -0,0 +1,17 @@
+package q13277;
+
+import java.util.*;
+import java.math.*;
+
+public class Q13277 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a, b;
+
+		a = sc.nextBigInteger();
+		b = sc.nextBigInteger();
+		sc.close();
+
+		System.out.println(a.multiply(b));
+	}
+}
