4c059f09af285e2d7d8ea6c29868f5a9e8af6bdc
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-18 04:01:54 +0900

    baekjoon: 15439
    백준 15439: Vera and Outfits

---
+++ Bronze V/src/q15439/Q15439.java
@@ -0,0 +1,16 @@
+package q15439;
+
+import java.util.*;
+
+public class Q15439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i;
+
+		i = sc.nextInt();
+		sc.close();
+
+		System.out.println(i * i - i);
+		// Vera의 상의와 바지를 곱하여 모든 경우의 수를 구하고, 색이 같은 경우를 뺀다.
+	}
+}
