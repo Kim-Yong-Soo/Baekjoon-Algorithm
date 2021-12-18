bdfaa77c4058da8ec7962569ab4b6cfe12c6456f
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-18 23:48:50 +0900

    baekjoon: 15740
    백준 15740: A+B - 9

---
+++ Bronze V/src/q15740/Q15740.java
@@ -0,0 +1,17 @@
+package q15740;
+
+import java.util.*;
+import java.math.*;
+
+public class Q15740 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a, b;
+
+		a = sc.nextBigInteger();
+		b = sc.nextBigInteger();
+		sc.close();
+
+		System.out.println(a.add(b));
+	}
+}
