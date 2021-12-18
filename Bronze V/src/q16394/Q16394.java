20904599e47e471f4dcd68aede6ef36884845147
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-19 00:59:58 +0900

    baekjoon: 16394
    백준 16394: 홍익대학교

---
+++ Bronze V/src/q16394/Q16394.java
@@ -0,0 +1,15 @@
+package q16394;
+
+import java.util.Scanner;
+
+public class Q16394 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+
+		n = sc.nextInt();
+		sc.close();
+
+		System.out.println(n - 1946);
+	}
+}
