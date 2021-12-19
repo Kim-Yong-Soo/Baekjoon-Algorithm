8c4bc1668033b3e6f0d21ce7128a71fd6bd8873a
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-19 23:33:36 +0900

    baekjoon: 17295
    백준 17295: 엔드게임 스포일러

---
+++ Bronze V/src/q17295/Q17295.java
@@ -0,0 +1,15 @@
+package q17295;
+
+import java.util.*;
+
+public class Q17295 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s;
+
+		s = sc.next();
+		sc.close();
+
+		System.out.println("Avengers: Endgame");
+	}
+}
