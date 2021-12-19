7782852a8c57f8afae94008a544a644fd38ef237
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-19 23:35:05 +0900

    baekjoon: 18108
    백준 18108: 1998년생인 내가 태국에서는 2541년생?!

---
+++ Bronze V/src/q18108/Q18108.java
@@ -0,0 +1,15 @@
+package q18108;
+
+import java.util.*;
+
+public class Q18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y;
+
+		y = sc.nextInt();
+		sc.close();
+
+		System.out.println(y - 543);
+	}
+}
