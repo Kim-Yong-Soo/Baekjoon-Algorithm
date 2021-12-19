eef6b6bd7bff79d0cfd97147dd57824ae0a031a3
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-19 23:34:25 +0900

    baekjoon: 18096
    백준 18096: Арифметическая магия

---
+++ Bronze V/src/q18096/Q18096.java
@@ -0,0 +1,18 @@
+package q18096;
+
+import java.util.*;
+
+public class Q18096 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n, res = 1;
+
+		n = sc.nextInt();
+		sc.close();
+
+		for (int i = 0; i < n; i++)
+			res *= 1;
+
+		System.out.println(res);
+	}
+}
