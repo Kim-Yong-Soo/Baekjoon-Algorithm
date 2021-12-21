2dfd4f21c0010b3484b5df6cf4644c50935925d9
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-21 23:49:09 +0900

    baekjoon: 20254
    백준 20254: Site Score

---
+++ Bronze V/src/q20254/Q20254.java
@@ -0,0 +1,18 @@
+package q20254;
+
+import java.util.*;
+
+public class Q20254 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ur, tr, uo, to;
+
+		ur = sc.nextInt();
+		tr = sc.nextInt();
+		uo = sc.nextInt();
+		to = sc.nextInt();
+		sc.close();
+
+		System.out.println((56 * ur) + (24 * tr) + (14 * uo) + (6 * to));
+	}
+}
