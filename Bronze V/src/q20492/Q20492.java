fe70b151f8f80f4391a72b817dc98c324d0787eb
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2021-12-22 23:41:37 +0900

    baekjoon: 20492
    백준 20492: 세금

---
+++ Bronze V/src/q20492/Q20492.java
@@ -0,0 +1,15 @@
+package q20492;
+
+import java.util.*;
+
+public class Q20492 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+
+		n = sc.nextInt();
+		sc.close();
+
+		System.out.println((int) (n * 0.78) + "\n" + (int) (n * 0.956));
+	}
+}
