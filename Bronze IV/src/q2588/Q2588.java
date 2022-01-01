bd41bdc569344b01d34f95c001e3bc09fa1416d0
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-01 23:57:47 +0900

    baekjoon: 2558
    백준 2558: 곱셈

---
+++ Bronze IV/src/q2588/Q2588.java
@@ -0,0 +1,19 @@
+package q2588;
+
+import java.util.Scanner;
+
+public class Q2588 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b / 10) % 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+	}
+}
