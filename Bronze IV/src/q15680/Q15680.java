454fa8b4ad3ee2b1fde4d3a5ecb9eb8302d0d929
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-03 23:45:04 +0900

    baekjoon: 15680
    백준 15680: 연세대학교

---
+++ Bronze IV/src/q15680/Q15680.java
@@ -0,0 +1,15 @@
+package q15680;
+
+import java.util.Scanner;
+
+public class Q15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+
+		n = sc.nextInt();
+		sc.close();
+
+		System.out.println((n == 0) ? "YONSEI" : "Leading the Way to the Future");
+	}
+}
