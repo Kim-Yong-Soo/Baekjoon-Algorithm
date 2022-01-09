0683dc58e8106f41dec15466e21d3569f533940f
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-09 21:52:29 +0900

    baekjoon: 24078
    백준 24078: 余り (Remainder)

---
+++ Bronze V/src/q24078/Q20478.java
@@ -0,0 +1,15 @@
+package q24078;
+
+import java.util.Scanner;
+
+public class Q20478 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x;
+
+		x = sc.nextInt();
+		sc.close();
+
+		System.out.println(x % 21);
+	}
+}
