fe66e09a9bdf43bf358335331ef1123313f3eb13
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-09 21:52:51 +0900

    baekjoon: 24082
    백준 24082: 立方体 (Cube)

---
+++ Bronze V/src/q24082/Q24082.java
@@ -0,0 +1,15 @@
+package q24082;
+
+import java.util.Scanner;
+
+public class Q24082 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x;
+
+		x = sc.nextInt();
+		sc.close();
+
+		System.out.println(x * x * x);
+	}
+}
