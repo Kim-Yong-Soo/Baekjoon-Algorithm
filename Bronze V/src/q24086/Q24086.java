6f0a22c734b7b947c538a9c5b9ac669f68f06aa3
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-09 21:53:32 +0900

    baekjoon: 24086
    백준 24086: 身長 (Height)

---
+++ Bronze V/src/q24086/Q24086.java
@@ -0,0 +1,16 @@
+package q24086;
+
+import java.util.Scanner;
+
+public class Q24086 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+
+		System.out.println(b - a);
+	}
+}
