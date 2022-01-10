0749eab091c8606be007989f9643fc1927350b5a
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-10 23:52:47 +0900

    baekjoon: 5596
    백준 5596: 시험 점수

---
+++ Bronze IV/src/q5596/Q5596.java
@@ -0,0 +1,16 @@
+package q5596;
+
+import java.util.Scanner;
+
+public class Q5596 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] sum = new int[2];
+
+		for (int i = 0; i < sum.length; i++) {
+			for (int j = 0; j < 4; j++)
+				sum[i] += sc.nextInt();
+		}
+		System.out.println((sum[0] == sum[1]) ? sum[0] : Math.max(sum[0], sum[1]));
+	}
+}
