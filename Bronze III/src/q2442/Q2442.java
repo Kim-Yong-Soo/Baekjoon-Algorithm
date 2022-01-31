4ff4692b2f2f7a81a2fecd527ab9d4af613e7d5e
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-01-31 21:05:34 +0900

    baekjoon: 2442
    백준 2442: 별 찍기 - 5

---
+++ Bronze III/src/q2442/Q2442.java
@@ -0,0 +1,28 @@
+package q2442;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2442 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n;
+
+		n = Integer.parseInt(br.readLine());
+		br.close();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < n - i; j++)
+				bw.write(" ");
+			for (int k = 0; k < 2 * i - 1; k++)
+				bw.write("*");
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
