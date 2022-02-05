6d633edf4233b2c58f88800f6794777396f60b9f
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-02-05 23:58:13 +0900

    baekjoon: 2523
    백준 2523: 별 찍기 - 13

---
+++ Bronze III/src/q2523/Q2523.java
@@ -0,0 +1,34 @@
+package q2523;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2523 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n;
+		
+		n = Integer.parseInt(br.readLine());
+		br.close();
+
+		for (int i = 1; i <= n; i++) {
+			bw.write("*");
+			for (int j = 1; j < i; j++)
+				bw.write("*");
+			bw.write("\n");
+		}
+
+		for (int i = n; i > 1; i--) {
+			bw.write("*");
+			for (int j = 2; j < i; j++)
+				bw.write("*");
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
