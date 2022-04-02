0763def858e75023b4f4919c366996de7cfde8e0
Kim-Yong-Soo <47918867+Kim-Yong-Soo@users.noreply.github.com>
2022-04-03 01:31:46 +0900

    baekjoon: 13163
    백준 13163: 닉네임에 갓 붙이기

---
+++ Bronze II/src/q13163/Q13163.java
@@ -0,0 +1,33 @@
+package q13163;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q13163 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n;
+
+		n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			bw.write("god");
+			while (st.hasMoreTokens())
+				bw.write(st.nextToken());
+			bw.write("\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
